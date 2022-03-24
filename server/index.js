b073eda126f8b1c830e164a21eef70aaf3e42786
Mintxshi <pikas.alt.mail@gmail.com>
2022-03-24 18:38:45 -0400

    my poosay stank

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const royalblue = chalk.hex('#3f51b5'); // Orange color
 const pink = chalk.hex('e91e63');
 const logSymbols = require('log-symbols');
 const green = chalk.hex('4caf50')
-var msg = "pneumonoultramicroscopicsilicovolcanoconiosis";    
+const logUpdate = require('log-update');
+const axios = require('axios');
+var msg = "pneumonoultramicroscopicsilicovolcanoconiosis";  
 app.get('/', (req, res) => {
     res.send(msg)
 })
@@ -15,10 +17,24 @@ app.get('/api/:txt', (req, res) => {
         console.log(logSymbols.success + " " + req.params.txt)
         msg = req.params.txt
     }else{
-        res.send('get the FUCK out')
+        res.status(403).jsonp({ error: "Permission Denied. Your request has been logged to the server and has been acknowledged." })
         console.log(logSymbols.warning + " " + req.params.txt + " " + req.ip)
 
     }
 })
-
-app.listen(3000, () => console.log(logSymbols.success + `Conduit@vxnwtf has started sucessfully at ${royalblue.bold('http://localhost:3000')}`))
+app.get('/online', (req, res) => {
+    res.send(true)
+})
+console.clear()
+axios.get('https://api64.ipify.org?format=json')
+  .then(function (response) {
+    // handle success
+    app.listen(3000, () => console.log(logSymbols.success + ` Conduit@vxnwtf has started sucessfully.\n ${logSymbols.info} URL ${royalblue.bold('http://localhost:3000')}\n ${logSymbols.info} IP: ${response.data.ip}\n`))
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
