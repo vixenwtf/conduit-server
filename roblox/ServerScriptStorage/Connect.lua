4d5958e5be5404d7fcda0e522f223bf676977898
Mintxshi <47258458+Mintxshi@users.noreply.github.com>
2022-03-27 17:43:21 -0400

    Delete roblox directory

--- roblox/ServerScriptStorage/Connect.lua
+++
@@ -1,3 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Resources = require(ReplicatedStorage:WaitForChild("Resources"))
-Resources:LoadLibrary("ReplicatedPseudoInstance")
